0229d5ec85fa4703f08a1aac3684e9f085ca33f9
Luis <luismarcelo.v.c@hotmail.com>
2024-03-06 19:56:59 -0300

    Aplicacion Con Web Socket

---
+++ src/main/java/com/WebSocket/Controller/WebSocketController.java
@@ -0,0 +1,20 @@
+package com.WebSocket.Controller;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.WebSocket.Models.ChatMessage;
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/chat/{roomId}")
+    @SendTo("/topic/{roomId}")
+    public ChatMessage chat(@DestinationVariable String roomId,ChatMessage message)
+    {
+        return new ChatMessage(message.getMessage(), message.getUser());
+    }
+    
+}
---
+++ src/main/java/com/WebSocket/Models/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.WebSocket.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ChatMessage {
+    
+    String message;
+    String user;
+}
